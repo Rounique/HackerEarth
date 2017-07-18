d2ea7c54b7b9c1b6fefecf2a97756fab5e3d4114
Rounique <roonakmoasses@gmail.com>
2017-07-18 17:39:18 +0430

    Update Life, the Universe, and Everything solution.cpp

--- Code Monk/Life, the Universe, and Everything solution.cpp
+++ Code Monk/Life, the Universe, and Everything solution.cpp
@@ -3,10 +3,11 @@ using namespace std;
 int main()
 {
     int a = 0;
-    while( a! = 42 )
+    while( a != 42 )
     {   
     	cin >> a;
     	if(a != 42)
     	cout << a << endl;
    	}
+   	return 0;
 } 
