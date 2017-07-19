56928c7aa5b9d24fd0bf956cf1ce57a6a8175048
Rounique <roonakmoasses@gmail.com>
2017-07-19 04:47:47 +0430

    Create Little Monk and Good String solution2.cpp

---
+++ Code Monk/Little Monk and Good String solution2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s; cin >> s;
+    int Max_LGS = 0;
+    int LGS = 0;
+    for ( int i = 0; i < s.size() ; i++ )
+    {
+        if ( s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || 
+        s[i] == 'o' || s[i] == 'u' )
+            LGS++;
+            
+        else LGS = 0;
+        if( LGS > Max_LGS )
+            Max_LGS = LGS ;
+    }
+    cout << Max_LGS;
+    return 0;
+}
